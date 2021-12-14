55e061d74af0e7a04be525643fb7dc70b6582f09
Spencer Walker <swalker2172@gmail.com>
2021-12-13 20:44:55 -0700

    yep

--- InventoryApp/models.py
+++ InventoryApp/models.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 # Create your models here.
 
 class Inventory(models.Model) :
-    productID = models.IntegerField()
+    productID = models.AutoField(primary_key=True)
     codigo = models.CharField(max_length = 30)
     marca = models.CharField(max_length = 30)
     descripcion = models.CharField(max_length = 100)
