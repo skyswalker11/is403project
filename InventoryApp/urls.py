78b95f9da68b56b54304b72f13af70c057d912bc
jacobgifford22 <90434868+jacobgifford22@users.noreply.github.com>
2021-12-14 00:54:18 -0700

    fixed edit

--- InventoryApp/urls.py
+++ InventoryApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import indexPageView, inventoryPageView, editPageView, createPageView, productDetailsPageView
+from .views import indexPageView, inventoryPageView, editPageView, createPageView, productDetailsPageView, putPageView
 
 urlpatterns = [
     path("", indexPageView, name = "index"), 
     path("inventory/", inventoryPageView, name = "inventory"),
     path("edit/<int:product_id>", editPageView, name = "edit"),
+    path("put/", putPageView, name = "put"),
     path("create/", createPageView, name = "create"),
     path("productdetails/<int:product_id>", productDetailsPageView, name = "productdetails")
 ]
--- InventoryApp/views.py
+++ InventoryApp/views.py
@@ -32,15 +32,40 @@ def editPageView(request, product_id) :
     if request.method == 'POST' :
         product_id = request.POST['product_id']
         product = Inventory.objects.get(product_id=product_id)
-        
+
         context = {
-            "product": product
+            "inventory": product
         }
 
-        return render(request, 'inventorypages/inventory.html', context)
+        return render(request, 'inventorypages/edit.html', context)
 
     else :
-        return render(request, 'inventorypages/edit.html')
+        return inventoryPageView(request)
+
+# View for posting edits
+def putPageView(request) :
+    if request.method == 'POST' :
+        product_id = request.POST['product_id']
+        codigo = request.POST['codigo']
+        marca = request.POST['marca']
+        descripcion = request.POST['descripcion']
+        ubicacion = request.POST['ubicacion']
+        unidad = request.POST['unidad']
+        stock = request.POST['stock']
+        dolares = request.POST['dolares']
+        soles = request.POST['soles']
+        valor_del_stock = request.POST['valor_del_stock']
+        tipo_de_cambio = request.POST['tipo_de_cambio']
+        
+        instance = Inventory(product_id=product_id, codigo=codigo, marca=marca, descripcion=descripcion, ubicacion=ubicacion, unidad=unidad, \
+            stock=stock, dolares=dolares, soles=soles, valor_del_stock=valor_del_stock, tipo_de_cambio=tipo_de_cambio)
+
+        instance.save()
+
+        return productDetailsPageView(request, product_id)
+
+    else :
+        return inventoryPageView(request)
 
 # View for creating new inventory page
 def createPageView(request) : 
